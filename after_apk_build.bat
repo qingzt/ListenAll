20c7e34cb32f01c6a7bb74355e87e1626723e455
qingzt <754307254@qq.com>
2024-07-08 23:47:05 +0800

    [ADD] Github Actions

---
+++ after_apk_build.bat
@@ -0,0 +1,21 @@
+@echo off
+setlocal enabledelayedexpansion
+
+del /f /s /q *sha1*.*
+del /f /s /q *debug*.*
+
+rem 遍历当前目录中的所有 .apk 文件
+for /f %%f in ('dir /b *.apk') do (
+    rem 提取文件名中的架构部分
+    set "filename=%%~nf"
+    set "arch=!filename:app-=!"
+    set "arch=!arch:-release=!"
+
+    rem 构建新的文件名
+    set "newname=listenall-!arch!-android.apk"
+
+    rem 重命名文件
+    ren "%%f" "!newname!"
+)
+
+endlocal
