5c4e25c527a0f998601058ab8aae47080f9c00e6
qingzt <754307254@qq.com>
2024-07-25 16:25:53 +0800

    fix: 记忆的播放模式在初次打开时不展示

--- plugins/floating_lyric/get.cpp
+++ plugins/floating_lyric/get.cpp
@@ -4,18 +4,36 @@ template <typename T>
 std::shared_ptr<T> Get::get()
 {
     auto controller=controllers[std::string(typeid(T).name())];
-    if(controller.has_value())
+    if(controller!=nullptr)
     {
         return std::any_cast<std::shared_ptr<T>>(controller);
     }
     return nullptr;
 }
 
-template <typename T>
-std::shared_ptr<T> Get::push(T controller)
+template <typename T, typename... Args>
+std::shared_ptr<T> Get::add(bool &inMemory,Args&&... args)
 {
     static_assert(std::is_base_of<GetController, T>::value, "T must be derived from GetController");
-    auto controller=std::make_shared<T>(controller);
-    controllers[std::string(typeid(T).name())]=controller;
-    return controller;
+    auto res=get<T>();
+    if (res!=nullptr){
+        if(inMemory){
+            return res;
+        }
+        else{
+            controllers.erase(std::string(typeid(T).name()));
+            return nullptr;
+        }
+    }
+    else{
+        if(inMemory){
+            auto controller=std::make_shared<T>(std::forward<Args>(args)...);
+            controllers[std::string(typeid(T).name())]=controller;
+            return controller;
+        }
+        else{
+            return nullptr;
+        }
+    }
+    return nullptr;
 }
